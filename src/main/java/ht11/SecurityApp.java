fb1888057ba4d8bc70d81d3556963297e85ee32d
Mr_Twister <kozminykhyura@yandex.ru>
2021-03-22 15:37:41 +0300

    some fixes

---
+++ src/main/java/ht11/SecurityApp.java
@@ -0,0 +1,13 @@
+package ht11;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityApp.class, args);
+	}
+
+}
